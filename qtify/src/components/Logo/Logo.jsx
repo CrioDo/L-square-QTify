d4a9d4dc55e0ed08b94a9d54c4b5686acd1361e7
Priyanka Chourey <priyanka.choure10@gmail.com>
2023-03-05 15:45:01 +0530

    C1: Navbar

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Logo = () => {
+  return <img src={require("../../assets/logo.png")} alt="logo" />;
+};
---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Logo } from "../Logo/Logo";
+import styles from "./Navbar.module.css";
+
+export const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+      <Logo />
+    </div>
+  );
+};
+
+//cafc rafc
