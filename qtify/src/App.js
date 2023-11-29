2db3d34563fdb6d5d4e7f9ed6293660045d767db
Mrityunjay Kumar <mk10399@gmail.com>
2023-11-29 22:39:26 +0530

    card grid, album, and songs section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import HeroSection from "./components/HeroSection/HeroSection";
+import Card from "./components/Card/Card";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import AlbumDetails from "./pages/AlbumDetails/AlbumDetails";
 
 function App() {
 	return (
 		<div className="App">
-      <NavBar />
-      <HeroSection />
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+
+				<Route path="/album/:slug" element={<AlbumDetails />} />
+
+				<Route path="*" element={<LandingPage />} />
+			</Routes>
 		</div>
 	);
 }
