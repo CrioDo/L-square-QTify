6d600a522d443b6324baa049f0a0ab77e0e5ca77
gurpreet1961 <gurpreet012guru@gmail.com>
2023-12-16 21:41:40 +0530

    C4: Search Bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Search from './Components/Search/Search';
 import Button from './Components/Button/Button';
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Button>Give Feedback</Button>
+      <Search placeholder='Search a song of your choice' />
     </>
   );
 }
