1aa5b606ed23b6430a05f2204cb078814d26c2e6
Prem <premkhodke.99@gmail.com>
2023-12-18 15:38:34 +0530

    Search and feedback

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "./components/Button/Button";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button>Give Feedback</Button>
+      <Search placeholder={"Search a album of your choice"} />
     </div>
   );
 }
---
+++ qtify/src/components/Search/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ReactComponent as SearchIcon } from '../../assets/search-icon.svg';
+import styles from "./Search.module.css";
+
+function Search({ placeholder }) {
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <form className={styles.wrapper} onSubmit={onSubmit} >
+            <input className={styles.search} required placeholder={placeholder} />
+            <button className={styles.searchButton} type='submit'>
+                <SearchIcon />
+            </button>
+        </form>
+    )
+}
+
+export default Search
