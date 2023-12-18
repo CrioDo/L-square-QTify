54ba1524aa3ae9f0c9d58a3e7c09d6e527e63608
Prem <premkhodke.99@gmail.com>
2023-12-18 13:44:03 +0530

    Test commit

---
+++ qtify/src/components/Feedback.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Button} from "@mui/material";
+
+
+function Feedback() {
+  return (
+    <div>
+        <Button>Feedback</Button>
+    </div>
+  )
+}
+
+export default Feedback
