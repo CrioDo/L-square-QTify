182e2270deb127f51deb88e0e67a4ccb3a631887
Avinash <avin.roy001@gmail.com>
2024-01-25 01:07:00 +0530

    my changes

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { SfNav } from 'react-sf-building-blocks';
 function App() {
   return (
-    <div className="App">
+    <div>
+      <div>
+            <SfNav showBanner={true} bannerText={"30% OFF On Select Products 💰"} bannerTextMobile={"30% OFF 💰"} />
+        </div>
       <h1>hello this is first time deployment</h1>
+
     </div>
   );
 }
