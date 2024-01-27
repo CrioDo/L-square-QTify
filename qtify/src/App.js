afca2f2c8b700e411273692ad183c4f4bda3fe11
Avinash <avin.roy001@gmail.com>
2024-01-27 23:33:09 +0530

    Hero section commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,9 +1,12 @@
 import styles from './App.module.css';
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Hero from './components/Hero/Hero';
+
 function App() {
   return (
     <div >
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
