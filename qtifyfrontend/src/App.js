86ffb395132ccebef7b10f43968833005e07e9a1
sanketdreja <sanketdreja@gmail.com>
2024-02-04 17:56:34 +0530

    Hero Section

--- qtifyfrontend/src/App.js
+++ qtifyfrontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import HeroSection from "./Components/HeroSection/HeroSection";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
