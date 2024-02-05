583b5e03a7c2067c1074383ec50df805312504f2
1919SaiRam <147429917+1919SaiRam@users.noreply.github.com>
2024-02-05 11:13:55 +0530

    Update Navbar.jsx

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
-import Search from "../Search/search";
+import Search from "../Search/Search";
 import Button from "../Button/Button";
 
 function Navbar({searchData}){
