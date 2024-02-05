822d6c1feafec884f1b4b99028b45ee3326e9a90
1919SaiRam <vijaykumarchinta1919@gmail.com>
2024-02-05 12:12:52 +0530

    mychanges

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { Outlet } from "react-router-dom"; 
 import Navbar from "./components/Navbar/Navbar";
 import { fetchNewAlbums, fetchSongs, fetchTopAlbums } from "./api/api";
 import { useEffect, useState } from "react";
+
 
 function App() {
   // return (
--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
-import Search from "../Search/search";
+import Search from "../Search/Search";
 import Button from "../Button/Button";
+import Link from "react-router-dom";
+
 
 function Navbar({searchData}){
     return (
