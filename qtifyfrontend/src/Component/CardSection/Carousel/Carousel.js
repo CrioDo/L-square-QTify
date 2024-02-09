0ad3b81dc2e7dbe7bab5f14e9d49fdc3eac2fc6e
sanketdreja <sanketdreja@gmail.com>
2024-02-10 03:22:10 +0530

    tabs

--- qtifyfrontend/src/Component/CardSection/Carousel/Carousel.js
+++ qtifyfrontend/src/Component/CardSection/Carousel/Carousel.js
@@ -20,7 +20,7 @@ function Carousel({ data, type }) {
       <Swiper
         initialSlide={0}
         modules={{ Navigation }}
-        slidesPerView={7}
+        slidesPerView={"auto"}
         spaceBetween={0}
         allowTouchMove
       >
