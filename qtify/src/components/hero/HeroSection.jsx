011265ef68cfbacbb15dab5ca808639b5fdb2855
swethamarimuthu <swethasubi12@gmail.com>
2024-03-27 21:03:01 +0530

    test

--- qtify/src/components/hero/HeroSection.jsx
+++ qtify/src/components/hero/HeroSection.jsx
@@ -1,8 +1,8 @@
-import heroImg from "../../assets/hero_headphones.svg";
+import heroImg from "../../assets/hero_headphones.png";
 import styles from "./hero.module.css";
 export default function HeroSection({ textLine1, textLine2, image }) {
   return (
-   
+    
 
     <div className={styles.heroBox}>
       <div className={styles.heroContent}>
@@ -11,7 +11,7 @@ export default function HeroSection({ textLine1, textLine2, image }) {
           <div>{textLine2}s</div>
         </div>
 
-        <img src={heroImg} alt="hero image" className={styles.heroImg} />
+        <img src={image} alt="hero image" className={styles.heroImg} />
       </div>
     </div>
   );
