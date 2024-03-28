0f32d6d19eedfe0d993e3ca7891135d7632bef21
swethamarimuthu <swethasubi12@gmail.com>
2024-03-28 18:03:36 +0530

    test

--- qtify/src/components/cardSwipper/cardSwiper.js
+++ qtify/src/components/cardSwipper/cardSwiper.js
@@ -6,7 +6,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
 
-import "./swiper.css";
 
 // import required modules
 import { useSwiper } from "swiper/react";
